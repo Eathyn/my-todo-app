2ebec6626c6c2e56ebd12252c27199da755f006f
Eathyn <eathynzakas@gmail.com>
2021-01-07 16:29:29 +0800

    add taskOption icon

--- web/src/views/middle/TaskAdder.vue
+++ web/src/views/middle/TaskAdder.vue
@@ -1,10 +1,16 @@
 <template>
-  <input type="text" name="name" v-model="model.name"
-         @keyup.enter="addTask" placeholder="添加任务，回车即可保存">
+  <div id="taskAdder">
+    <input type="text" name="name" v-model="model.name"
+           @keyup.enter="addTask" placeholder="添加任务，回车即可保存">
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-calendar-fill"></use>
+    </svg>
+  </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import taskOptions from '../../assets/icons/taskOptions'
 
 export default {
   name: 'TaskAdder',
@@ -34,7 +40,7 @@ export default {
 
 <style scoped>
 input {
-  width: 670px;
+  width: 630px;
   height: 40px;
   padding: 0 12px;
   font-size: 14px;
@@ -42,5 +48,13 @@ input {
   border-radius: 4px;
   outline: none;
   border: 1px solid transparent;
+}
+.icon {
+  width: 1.5em;
+  height: 1.5em;
+  padding-left: 20px;
+  vertical-align: -0.35em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
