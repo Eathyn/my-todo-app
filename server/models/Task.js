693fea43a9fb824d99d8f3b12a47d574b0466281
Eathyn <eathynzakas@gmail.com>
2021-01-07 16:30:37 +0800

    add task options in taskSchema

--- server/models/Task.js
+++ server/models/Task.js
@@ -4,6 +4,28 @@ const taskSchema = mongoose.Schema({
   name: {
     type: String,
   },
+  date: {
+    year: {
+      type: Number,
+    },
+    month: {
+      type: Number,
+    },
+    day: {
+      type: Number,
+    },
+  },
+  period: {
+    startingTime: {
+      type: String,
+    },
+    endTime: {
+      type: String,
+    },
+  },
+  duration: {
+    type: Number,
+  },
 })
 
 module.exports = mongoose.model('Task', taskSchema)
