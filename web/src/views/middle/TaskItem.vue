7921a680f95e9ef1be38a0f21e668133a84df263
Eathyn <eathynzakas@gmail.com>
2021-01-08 21:21:57 +0800

    import task play, suspended and end icons

--- web/src/views/middle/TaskItem.vue
+++ web/src/views/middle/TaskItem.vue
@@ -1,9 +1,23 @@
 <template>
-  <li @contextmenu.prevent="showTaskMenu">{{ taskItem.name }}</li>
+  <li @contextmenu.prevent="showTaskMenu">
+    {{ taskItem.name }}
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-play"></use>
+    </svg>
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-suspended"></use>
+    </svg>
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-stop"></use>
+    </svg>
+  </li>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import taskPlay from '../../assets/icons/taskPlay'
+import taskEnd from '../../assets/icons/taskEnd'
+import taskSuspended from '../../assets/icons/taskSuspended'
 
 export default {
   name: 'TaskItem',
@@ -30,7 +44,7 @@ export default {
 <style scoped>
 li {
   list-style-type: none;
-  width: 635px;
+  width: 600px;
   height: 35px;
   line-height: 2;
   padding: 0 12px;
@@ -43,5 +57,12 @@ li:hover {
 li:focus {
   background-color: #eef0fb;
   outline: none;
+}
+.icon {
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: -0.4em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
