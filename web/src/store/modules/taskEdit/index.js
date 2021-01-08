adc3b3853c0c33f2a42b6de2ae24f13ebc635fdb
Eathyn <eathynzakas@gmail.com>
2021-01-08 20:59:24 +0800

    add task edit toggle

--- web/src/store/modules/taskEdit/index.js
+++ web/src/store/modules/taskEdit/index.js
@@ -1,17 +1,21 @@
 const state = {
-
+  taskEditToggle: false
 }
 
 const mutations = {
-
+  UPDATE_TASK_EDIT_TOGGLE(state, toggle) {
+    state.taskEditToggle = toggle
+  }
 }
 
 const actions = {
-
+  updateTaskEditToggle({ commit }, toggle) {
+    commit('UPDATE_TASK_EDIT_TOGGLE', toggle)
+  }
 }
 
 const getters = {
-
+  taskEditToggle: state => state.taskEditToggle
 }
 
 const taskEditModule = {
