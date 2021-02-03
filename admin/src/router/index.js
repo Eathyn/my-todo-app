b0486b99844c05e390147b3ce3d23f1238852e84
Eathyn <eathynzakas@gmail.com>
2021-02-03 10:16:35 +0800

    feat(index.js): add routes
    add /register, /login and / routes

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Register from '../views/Register'
+import Login from '../views/Login'
+import Main from '../views/Main'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/register',
+    component: Register,
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/',
+    component: Main,
+  },
 ]
 
 const router = new VueRouter({
