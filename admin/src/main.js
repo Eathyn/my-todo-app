59a20c0152aec734dcea59eee0d39f3675752eb6
Eathyn <eathynzakas@gmail.com>
2021-02-03 09:54:24 +0800

    feat(main.js): import router and http

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import router from './router'
+import http from './http'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = http
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
