0cb6c220add463e37d0824742018d7f979fc1c81
Eathyn <eathynzakas@gmail.com>
2021-02-03 09:45:24 +0800

    build(http.js): add axios
    Using axios to implement CORS. Add request and response interceptors

---
+++ admin/src/http.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: 'http://localhost:3000/admin/api'
+})
+
+// axios request interceptor
+http.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${localStorage.token}`
+  return config
+}, err => {
+  return Promise.reject(err)
+})
+
+// axios response interceptor
+http.interceptors.response.use(res => {
+  return res
+}, err => {
+  alert(err.response.data.message)
+  return Promise.reject(err)
+})
+
+export default http
