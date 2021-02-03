a8df078bd9ae9e8af5696e734900b2cf977a9390
Eathyn <eathynzakas@gmail.com>
2021-02-03 11:45:45 +0800

    feat(Main.vue): add Main.vue

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="col1">
+      <div class="user">
+        <div>用户管理</div>
+        <div class="actions">
+          <router-link to="/user/add">增加用户</router-link>
+          <div>删除用户</div>
+          <div>修改用户</div>
+        </div>
+      </div>
+      <div class="admin">
+        <div>管理员管理</div>
+        <div class="actions">
+          <router-link to="/admin/add">增加管理员</router-link>
+          <div>删除管理员</div>
+          <div>修改管理员</div>
+        </div>
+      </div>
+    </div>
+    <router-view class="col2"></router-view>
   </div>
 </template>
 
@@ -11,5 +29,17 @@ export default {
 </script>
 
 <style scoped>
-
+.container {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+.col1 {
+  background-color: indianred;
+  width: 15%;
+}
+.col2 {
+  background-color: #aaa9a9;
+  width: 85%;
+}
 </style>
