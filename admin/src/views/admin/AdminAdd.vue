d7ebc4992ff6f72227f866c17aefc3eb32ed1468
Eathyn <eathynzakas@gmail.com>
2021-02-03 16:44:47 +0800

    feat: add AdminAdd component

---
+++ admin/src/views/admin/AdminAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label for="email">邮箱</label><input type="email" id="email" name="email" v-model="model.email">
+    <label for="name">管理员名称</label><input type="text" id="name" name="id" v-model="model.name">
+    <label for="password">密码</label><input type="password" id="password" name="password" v-model="model.password">
+    <button @click="submit">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminAdd',
+  data() {
+    return {
+      model: {
+        email: '',
+        name: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    submit() {
+      this.$http.post('/adminAdd', this.model)
+      this.model.email = ''
+      this.model.name = ''
+      this.model.password = ''
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
