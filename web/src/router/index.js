5d725c2d9119310fa4196a9914395cf1b6c198ad
Eathyn <eathynzakas@gmail.com>
2021-03-02 21:44:39 +0800

    fix: add global route guard

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -32,4 +32,13 @@ const router = new VueRouter({
   routes,
 })
 
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (!token && to.path !== '/register' && to.path !== '/login') {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
 export default router
