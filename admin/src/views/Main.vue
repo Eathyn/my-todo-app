1c39351f8ed225a8329edd03f6dfd4c1f71adde2
Eathyn <eathynzakas@gmail.com>
2021-03-03 22:41:46 +0800

    feat: add router links

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -1,35 +1,72 @@
 <template>
   <div class="container">
-    <div class="col1">
-      <div class="personal">
-        <router-link to="/personal" tag="div">个人信息管理</router-link>
-      </div>
-      <div class="user">
-        <div>用户管理</div>
-        <div class="actions">
-          <router-link to="/user/add" tag="div">增加用户</router-link>
-          <router-link to="/user/edit" tag="div">编辑用户</router-link>
-        </div>
-      </div>
-      <div class="admin">
-        <div>管理员管理</div>
-        <div class="actions">
-          <router-link to="/admin/add" tag="div">增加管理员</router-link>
-          <router-link to="/admin/edit" tag="div">编辑管理员</router-link>
-        </div>
+    <div class="header">
+      <div>待办清单应用后台管理系统</div>
+      <div>
+        <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bussiness-man"></use></svg>
+        <span>管理员</span>
       </div>
     </div>
-    <div class="col2">
-      <nav>
-        <div>待办清单应用后台管理系统</div>
-        <div>管理员 : {{ admin.name }}</div>
-      </nav>
-      <router-view></router-view>
+    <div class="main">
+      <div class="col1">
+        <div class="personal">
+          <router-link to="/personal" tag="div" class="classification">
+            <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bussiness-man"></use></svg>
+            <span>个人信息管理</span>
+          </router-link>
+        </div>
+        <div class="user">
+          <div class="classification">
+            <svg class="icon" aria-hidden="true"><use xlink:href="#icon-Customermanagement"></use></svg>
+            <span>用户管理</span>
+          </div>
+          <div>
+            <router-link to="/user/add" tag="div" class="action">
+              <svg class="icon" aria-hidden="true"><use xlink:href="#icon-add-select"></use></svg>
+              <span>增加用户</span>
+            </router-link>
+            <router-link to="/user/edit" tag="div" class="action">
+              <svg class="icon" aria-hidden="true"><use xlink:href="#icon-editor"></use></svg>
+              <span>编辑用户</span>
+            </router-link>
+          </div>
+        </div>
+        <div class="admin">
+          <div class="classification">
+            <svg class="icon" aria-hidden="true"><use xlink:href="#icon-Customermanagement-fill"></use></svg>
+            <span>管理员管理</span>
+          </div>
+          <div>
+            <router-link to="/admin/add" tag="div" class="action">
+              <svg class="icon" aria-hidden="true"><use xlink:href="#icon-add-select"></use></svg>
+              <span>增加管理员</span>
+            </router-link>
+            <router-link to="/admin/edit" tag="div" class="action">
+              <svg class="icon" aria-hidden="true"><use xlink:href="#icon-editor"></use></svg>
+              <span>编辑管理员</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col2">
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import personalIcon from '../assets/icons/personal'
+// eslint-disable-next-line no-unused-vars
+import userIcon from '../assets/icons/user'
+// eslint-disable-next-line no-unused-vars
+import adminIcon from '../assets/icons/admin'
+// eslint-disable-next-line no-unused-vars
+import addIcon from '../assets/icons/add'
+// eslint-disable-next-line no-unused-vars
+import editIcon from '../assets/icons/edit'
+
 export default {
   name: 'Main',
   data() {
@@ -50,26 +87,68 @@ export default {
 </script>
 
 <style scoped>
+.icon {
+  width: 1.5em;
+  height: 1.5em;
+  padding-right: 5px;
+  vertical-align: -0.35em;
+  fill: currentColor;
+  overflow: hidden;
+}
 .container {
   display: flex;
-  width: 100%;
-  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fafafa;
+  box-sizing: border-box;
+  padding: 40px 0 0 0;
 }
-.col1 {
-  background-color: #667fdd;
-  width: 20%;
-  color: #e3e3e3;
-}
-.col2 {
-  background-color: #aaa9a9;
-  width: 80%;
-}
-
-nav {
+.header {
   display: flex;
   justify-content: space-between;
+  width: 60%;
   box-sizing: border-box;
-  padding: 30px 60px 30px 40px;
-  background-color: #5670d3;
+  padding: 0 70px 40px 70px;
+  font-size: 22px;
+}
+.main {
+  display: flex;
+  width: 60%;
+  height: 80vh;
+  border: 1px solid #f2f2f2;
+}
+.col1 {
+  background-color: #ffffff;
+  width: 25%;
+  color: #51545a;
+  box-sizing: border-box;
+  padding: 30px;
+  border-right: 1px solid #f2f2f2;
+}
+.col2 {
+  background-color: #ffffff;
+  width: 75%;
+  box-sizing: border-box;
+  padding: 30px;
+}
+
+.personal, .user, .admin {
+  box-sizing: border-box;
+  padding: 0 0 15px 0;
+}
+.classification, .action {
+  box-sizing: border-box;
+}
+.classification {
+  padding: 0 0 10px 0;
+}
+.action {
+  margin: 0 0 10px 10px;
+}
+
+.action:hover, .personal > div:hover {
+  font-weight: bold;
+  cursor: pointer;
 }
 </style>
