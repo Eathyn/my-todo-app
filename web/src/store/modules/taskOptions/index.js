803a58ddff3081d1a99ec7c9f942a067f82916db
Eathyn <eathynzakas@gmail.com>
2021-03-09 22:39:07 +0800

    fix: automatically set date to today

--- web/src/store/modules/taskOptions/index.js
+++ web/src/store/modules/taskOptions/index.js
@@ -1,5 +1,14 @@
+import { mapGetters } from 'vuex'
+
 const state = {
-  taskOptions: null,
+  taskOptions: {
+    date: '',
+    period: {
+      startingTime: '',
+      endTime: '',
+    },
+    duration: '',
+  },
 }
 
 const mutations = {
@@ -9,13 +18,21 @@ const mutations = {
 }
 
 const actions = {
-  updateTaskOptions({ commit }, taskOptions) {
+  updateTaskOptions({ commit }, taskOptions = state.taskOptions) {
+    const [ clickedListId, listOfTodayTasksId ] = [ this.getters.clickedList.id, this.getters.listOfTodayTasks.id ]
+    if (clickedListId === listOfTodayTasksId) {
+      taskOptions.date = this.getDateOfToday()
+    }
     commit('UPDATE_TASK_OPTIONS', taskOptions)
   },
 }
 
 const getters = {
   taskOptions: state => state.taskOptions,
+  getDateOfToday() {
+    const [year, month, day] = new Date().toLocaleDateString().split('/')
+    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`
+  }
 }
 
 const taskOptionsModule = {
