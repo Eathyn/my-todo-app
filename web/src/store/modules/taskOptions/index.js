7fbadeef6b033d0cda9750da713f3b1dec73ee95
Eathyn <eathynzakas@gmail.com>
2021-03-12 22:13:26 +0800

    refactor: move feature of automatically set date to another function

--- web/src/store/modules/taskOptions/index.js
+++ web/src/store/modules/taskOptions/index.js
@@ -1,5 +1,3 @@
-import { mapGetters } from 'vuex'
-
 const state = {
   taskOptions: {
     date: '',
@@ -19,20 +17,12 @@ const mutations = {
 
 const actions = {
   updateTaskOptions({ commit }, taskOptions = state.taskOptions) {
-    const [ clickedListId, listOfTodayTasksId ] = [ this.getters.clickedList.id, this.getters.listOfTodayTasks.id ]
-    if (clickedListId === listOfTodayTasksId) {
-      taskOptions.date = this.getDateOfToday()
-    }
     commit('UPDATE_TASK_OPTIONS', taskOptions)
   },
 }
 
 const getters = {
   taskOptions: state => state.taskOptions,
-  getDateOfToday() {
-    const [year, month, day] = new Date().toLocaleDateString().split('/')
-    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`
-  }
 }
 
 const taskOptionsModule = {
