713ab8c22252706aac65405e9c27b4e28732dba1
Eathyn <eathynzakas@gmail.com>
2021-03-13 21:43:56 +0800

    build: import Echarts

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import http from './http'
+import ECharts from 'vue-echarts'
 import App from './App.vue'
+import * as echarts from 'echarts'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = http
+Vue.component('v-chart', ECharts)
 
 new Vue({
   router,
